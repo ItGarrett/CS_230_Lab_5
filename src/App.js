5c2ddf62df13d404bf0d12768d52086e0c0895ba
ItGarrett <gpb00001@mix.wvu.edu>
2023-10-31 17:18:24 -0400

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import navbar from "./navbar";
+import card from "./card";
 
 function App() {
   return (
     <div className="App">
+
+      <navbar />
+
       <h1>CS 230L</h1>
       <h2>Section - 02</h2>
       <p>WVU ID: 800328658</p>
       <p>Hi I am Garrett Butler</p>
+
+      <card />
+
     </div>
   );
 }
