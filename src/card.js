62fee7be79a17e47ea311eb0d1c2838a0fdcd379
ItGarrett <gpb00001@mix.wvu.edu>
2023-11-06 15:17:23 -0500

    Lab6

--- src/card.js
+++ src/card.js
@@ -1,39 +1,33 @@
 function Card() {
     return (
-        <div classname="row row-cols-1 row-cols-md-3 g-4">
-  <div classname="col">
-    <div classname="card h-100">
-      <img src="..." classname="card-img-top" alt="..." />
-      <div classname="card-body">
-        <h5 classname="card-title">Card title</h5>
-        <p classname="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div classname="card-footer">
-        <small classname="text-body-secondary">Last updated 3 mins ago</small>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+  <div className="col">
+    <div className="card h-100 text-bg-primary">
+    <div class="card-header">
+      <h5 className="card-title">Card 01</h5>
+    </div>
+      <div className="card-body">
+        <p className="card-text">This is some example text to make up the body of the card.</p>
       </div>
     </div>
   </div>
-  <div classname="col">
-    <div classname="card h-100">
-      <img src="..." classname="card-img-top" alt="..." />
-      <div classname="card-body">
-        <h5 classname="card-title">Card title</h5>
-        <p classname="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div classname="card-footer">
-        <small classname="text-body-secondary">Last updated 3 mins ago</small>
+  <div className="col">
+    <div className="card h-100 text-bg-danger"> 
+    <div class="card-header">
+      <h5 className="card-title">Card 02</h5>
+    </div>
+      <div className="card-body">
+        <p className="card-text">This is some example text to make up the body of the card.</p>
       </div>
     </div>
   </div>
-  <div classname="col">
-    <div classname="card h-100">
-      <img src="..." classname="card-img-top" alt="..." />
-      <div classname="card-body">
-        <h5 classname="card-title">Card title</h5>
-        <p classname="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div classname="card-footer">
-        <small classname="text-body-secondary">Last updated 3 mins ago</small>
+  <div className="col">
+    <div className="card h-100 text-bg-success">
+    <div class="card-header">
+      <h5 className="card-title">Card 03</h5>
+    </div>
+      <div className="card-body">
+        <p className="card-text">This is some example text to make up the body of the card.</p>
       </div>
     </div>
   </div>
